ff220eaa1c5a92949a7c9c7983b089ae4fb6cab8
Vinojith <gunaratne.19@cse.mrt.ac.lk>
2024-05-31 00:05:30 +0530

    Two, three sum Problems

---
+++ Leetcode/Threesum.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+nums = [-1,0,1,2,-1,-4]
+
+
+
+def twoSum(nums,target)-> list[int]:
+
+    c = {}
+    for i in nums:
+        c[i] = i
+
+    for i in range(len(nums)):
+            if target- nums[i] in c and i != nums.index(target- nums[i]) and i!=nums.index(-target) and nums.index(-target)!=nums.index(target- nums[i]) :
+            # if target- nums[i] in c and i != nums.index(target- nums[i]) :
+                return [-target ,nums[i] , target- nums[i]]
+       
+# for i in range(len(a)):
+    
+for i in range(len(nums)-2):
+     target = nums[i]
+     print(twoSum(nums,-target))
+
+# print(set([-1, 0, 1]) == set([0, -1, 1]))
--- Leetcode/twosum.py
+++ Leetcode/twosum.py
@@ -1,21 +1,19 @@
 print("Hello World")
 
-a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-b = 20
+nums = [3,3]
+target = 6
 
 c = {}
 
-for i in range(len(a)):
-    c[a[i]] = a[i]
+c = {}
 
+for i in nums:
+    c[i] = i
 
-for i in a:
-    try:
-        # print(c[abs(b-i)])
-        if a.index(i) != a.index(abs(b-i)) : print(a.index(i),a.index(abs(b-i))) 
-    except ValueError as e :
-        continue
-
+for i in range(len(nums)):
+        if target- nums[i] in c and i != nums.index(target- nums[i]):
+             print([i, nums.index(target- nums[i])])
+       
 # for i in range(len(a)):
     
 print(c)
