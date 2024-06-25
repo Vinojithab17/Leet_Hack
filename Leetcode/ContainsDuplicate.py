fdfcd4676c43017f4d42d1f2fe59638dfcd1a6e9
Vinojith <gunaratne.19@cse.mrt.ac.lk>
2024-06-25 09:50:54 +0530

    refactor: Optimize containsNearbyDuplicate function in Leetcode

--- Leetcode/ContainsDuplicate.py
+++ Leetcode/ContainsDuplicate.py
@@ -1,19 +1,18 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        ans = []
+        dic = {}
+        dic2 = {}
         for i in range(len(nums)):
-            if nums[i] == k:
-                ans.append(i)
-        print(ans)
-        if len(ans)>=2:
-            if (ans[1]-ans[0]<=k):
-                return True
-        if len(ans)==1 :
-            return True
-        else: return False
-
+            if nums[i] not in dic:
+                dic[nums[i]] = i
+            else:
+                if abs(dic[nums[i]]-i)<=k:
+                    return True
+                else:
+                    dic[nums[i]] = i
+        return False
 s = Solution()
-print(s.containsNearbyDuplicate([1,0,1,1],1))
+print(s.containsNearbyDuplicate([1],1))
 
 # Wrong solution
 
