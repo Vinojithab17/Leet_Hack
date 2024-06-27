e9237084560e89be2857a185f8dab2e8bc2e8b12
Vinojith <gunaratne.19@cse.mrt.ac.lk>
2024-06-28 00:01:15 +0530

    feat: Add code for converting a string to a decimal number in Leetcode

--- Leetcode/hexa.py
+++ Leetcode/hexa.py
@@ -21,3 +21,13 @@ class Solution:
 
 s = Solution()
 print(s.toHex(26))
+
+print(ord('1')-48)
+print(ord('9')-48)
+
+
+n = "123"
+num=0
+for i in range(len(n)):
+    num += (ord(n[len(n)-1-i])-48)*10**i
+print(num)
