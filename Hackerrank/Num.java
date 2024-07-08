4222078d378a048df6654afdc724d0928e47dd06
Vinojith <gunaratne.19@cse.mrt.ac.lk>
2024-07-08 23:22:07 +0530

    Made some changes in Num file

--- Hackerrank/Num.java
+++ Hackerrank/Num.java
@@ -1,13 +1,17 @@
 
 public class Num {
     public static void main(String[] args) {
-
+        String s = "hello there";
+        System.out.println(s);
         int a = 1;
         int b = 1;
         int c = a++; // Assigns the value and increments
         int d = ++b; // increment and assigns the value
         System.out.println("The Value of c is = " + c);
         System.out.println("The Value of d is = " + d);
+        int num = 1;
+        System.out.println(++num);
+        System.out.println(num++);
         // The Value of c is = 1
         // The Value of d is = 2
 
